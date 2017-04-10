8007992a55309a22b37bb2662c66c418a6e52174
Robert Svensson <kane610@me.com>
2017-04-10 22:47:12 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name = 'axis',
+  packages = ['axis'],
+  version = '1',
+  description = 'A python library for communicating with Axis Communications devices',
+  author = 'Robert Svensson',
+  author_email = 'Kane610@users.noreply.github.com',
+  url = 'https://github.com/miniconfig/python-openevse-wifi',
+  download_url = 'https://github.com/miniconfig/python-openevse-wifi/tarball/0.4'
+)
