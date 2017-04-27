b4acb578c1c9c365154fe5f135f2e72f940db4ad
Robert Svensson <kane610@me.com>
2017-04-27 12:28:22 +0200

    Added test_device

---
+++ test_device.py
@@ -0,0 +1,10 @@
+from axis import AxisDevice
+
+config = {'alias': 'alias',
+          'name': 'name',
+          'host': '10.0.1.65',
+          'username': 'root',
+          'password': 'passs',
+          'location': 'location'}
+
+device = AxisDevice(config)
