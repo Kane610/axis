0f120b6ff1e4644604a8b684c91db695315e2391
Robert Svensson <kane610@me.com>
2017-04-28 18:26:29 +0200

    V4

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'axis',
   packages = ['axis'],
-  version = '3',
+  version = '4',
   description = 'A python library for communicating with devices from Axis Communications',
   author = 'Robert Svensson',
   author_email = 'Kane610@users.noreply.github.com',
   license = 'MIT',
   url = 'https://github.com/Kane610/axis',
-  download_url = 'https://github.com/Kane610/axis/archive/v3.tar.gz',
+  download_url = 'https://github.com/Kane610/axis/archive/v4.tar.gz',
   install_requires=['requests']
 )
