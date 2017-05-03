1168de1883cc79ac4d73c8582ba54aa154fde93d
Robert Svensson <kane610@me.com>
2017-05-03 10:45:50 +0200

    Trying to fix pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-  name = 'axis',
-  packages = ['axis'],
-  version = '6',
-  description = 'A python library for communicating with devices from Axis Communications',
-  author = 'Robert Svensson',
-  author_email = 'Kane610@users.noreply.github.com',
-  license = 'MIT',
-  url = 'https://github.com/Kane610/axis',
-  download_url = 'https://github.com/Kane610/axis/archive/v6.tar.gz',
+  name='axis',
+  packages=['axis'],
+  version='7',
+  description='A python library for communicating with devices from Axis Communications',
+  author='Robert Svensson',
+  author_email='Kane610@users.noreply.github.com',
+  license='MIT',
+  url='https://github.com/Kane610/axis',
+  download_url='https://github.com/Kane610/axis/archive/v7.tar.gz',
   install_requires=['requests']
 )
