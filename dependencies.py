9502de0e6be30e4592f4f0cf141abc27db64ccf4
Robert Svensson <kane610@me.com>
2017-06-05 12:05:54 +0200

    Clean up of text Proper exit when exception has been raised

--- dependencies.py
+++ dependencies.py
@@ -1,28 +1,32 @@
 import os
 import pkgutil
 import site
+from sys import exit
 
-if pkgutil.find_loader("gi"):
+if pkgutil.find_loader('gi'):
     try:
         import gi
-        print('Found gi:', os.path.abspath(gi.__file__))
+        print("Found gi at:", os.path.abspath(gi.__file__))
         gi.require_version('Gst', '1.0')
-        # from gi.repository import GLib, Gst
+        # from gi.repository import Gst
     except ValueError:
-        print('Couldn\'t find Gst')
-        print('Please run \'sudo apt-get install gir1.2-gstreamer-1.0\'')
-        return False
-    print('Environment seems to be ok.')
+        print("Couldn\'t find Gst",
+              '\n',
+              "Please run \'sudo apt-get install gir1.2-gstreamer-1.0\'")
+        exit(False)
+    print("Environment seems to be ok.")
 else:
-    print('No gi installed', '\n',
-          'Please run \'sudo apt-get install python3-gi\'',
+    print("No gi available in this environment",
           '\n',
-          'A virtual environment might need extra actions like symlinking, ',
+          "Please run \'sudo apt-get install python3-gi\'",
           '\n',
-          'you might need to do a symlink looking similar to this:',
+          "A virtual environment might need extra actions like symlinking,",
           '\n',
-          'ln -s /usr/lib/python3/dist-packages/gi ',
-          '/srv/homeassistant/lib/python3.4/site-packages',
+          "you might need to do a symlink looking similar to this:",
           '\n',
-          'run this script inside and outside of the virtual environment to find the paths needed')
+          "ln -s /usr/lib/python3/dist-packages/gi",
+          "/srv/homeassistant/lib/python3.4/site-packages",
+          '\n',
+          "run this script inside and outside of the virtual environment",
+          "to find the paths needed")
     print(site.getsitepackages())
