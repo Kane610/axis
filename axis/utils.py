8dc3042661c5731eb0b1087dc564253888c923cf
Robert Svensson <kane610@me.com>
2018-07-02 23:19:01 +0200

    Add new file utils that will keep some generic functionality

---
+++ axis/utils.py
@@ -0,0 +1,24 @@
+"""Python library to enable Axis devices to integrate with Home Assistant."""
+
+import logging
+import requests
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def session_request(session, url, **kwargs):
+    """Do HTTP/S request and return response as dictionary."""
+    try:
+        response = session(url, **kwargs)
+        response.raise_for_status()
+        if response.status_code != 200:
+            _LOGGER.error(
+                "HTTP status %d, response %s.", response.status, response.text)
+            return None
+    except requests.ConnectionError as err:
+        _LOGGER.error("Connection error: %s", err)
+        return None
+    except requests.exceptions.HTTPError as err:
+        _LOGGER.error("HTTP error: %s", err)
+        return None
+    return response.text
