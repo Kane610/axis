03cefafcccae818e1f1e13d67cf8b10d3d10e6e4
Robert Svensson <kane610@me.com>
2018-11-23 09:20:19 +0100

    Simplify Vapix requests

--- axis/vapix.py
+++ axis/vapix.py
@@ -10,15 +10,13 @@ _LOGGER = logging.getLogger(__name__)
 PARAM_URL = '{}://{}:{}/axis-cgi/{}?action={}&{}'
 
 # Param.cgi
-VAPIX_FW_VERSION = ('param.cgi', 'list', 'Properties.Firmware.Version')
-VAPIX_IMAGE_FORMAT = ('param.cgi', 'list', 'Properties.Image.Format')
-VAPIX_MDNS_FRIENDLY_NAME = (
-    'param.cgi', 'list', 'Network.Bonjour.FriendlyName')
-VAPIX_META_DATA_SUPPORT = (
-    'param.cgi', 'list', 'Properties.API.Metadata.Metadata')
-VAPIX_MODEL_ID = ('param.cgi', 'list', 'Brand.ProdNbr')
-VAPIX_PROD_TYPE = ('param.cgi', 'list', 'Brand.ProdType')
-VAPIX_SERIAL_NUMBER = ('param.cgi', 'list', 'Properties.System.SerialNumber')
+VAPIX_FW_VERSION = 'Properties.Firmware.Version'
+VAPIX_IMAGE_FORMAT = 'Properties.Image.Format'
+VAPIX_MDNS_FRIENDLY_NAME = 'Network.Bonjour.FriendlyName'
+VAPIX_META_DATA_SUPPORT = 'Properties.API.Metadata.Metadata'
+VAPIX_MODEL_ID = 'Brand.ProdNbr'
+VAPIX_PROD_TYPE = 'Brand.ProdType'
+VAPIX_SERIAL_NUMBER = 'Properties.System.SerialNumber'
 
 # Pwdgrp.cgi
 VAPIX_USER_LIST = ('pwdgrp.cgi', 'get', '')
@@ -30,6 +28,22 @@ class Vapix(object):
     def __init__(self, config):
         """Store local reference to device config."""
         self.config = config
+        self.params = {}
+
+    def load_params(self):
+        """"""
+        result = self.do_request('param.cgi', 'list', 'group=root')
+
+        self.params = {
+            key: value
+            for s in filter(None, result.split('\n'))
+            if '=' in s
+            for key, value in [s.split('=', 1)]
+        }
+
+    def get_param(self, param):
+        """"""
+        return self.params.get('root.' + param, '')
 
     def get(self, vapix_tuple):
         """"""
