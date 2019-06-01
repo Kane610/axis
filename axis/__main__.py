e65a08fdaf13e75be13c97a88415f833eb05c2ef
Robert Svensson <Kane610@users.noreply.github.com>
2019-06-01 22:10:44 +0200

    Improved main function (#15)

--- axis/__main__.py
+++ axis/__main__.py
@@ -1,21 +1,50 @@
-# python3 -m axis
+"""Read events and parameters from your Axis device."""
 
 import asyncio
+import argparse
+import logging
+import sys
 
 from axis import AxisDevice
-from functools import partial
 
 
-loop = asyncio.get_event_loop()
+async def main(args):
+    loop = asyncio.get_event_loop()
+    device = AxisDevice(
+        loop=loop, host=args.host, username=args.username,
+        password=args.password, port=args.port)
 
-port = 8080
-event_list = ['motion']
-kw = {'host': '10.0.1.51',
-      'username': 'root',
-      'password': 'pass',
-      'port': port,
-      'events': event_list}
+    if args.params:
+        await loop.run_in_executor(None, device.vapix.initialize_params)
+        await loop.run_in_executor(None, device.vapix.initialize_ports)
+        await loop.run_in_executor(None, device.vapix.initialize_users)
 
-loop.call_soon(partial(AxisDevice, loop, **kw))
-loop.run_forever()
-loop.close()
+        if not args.events:
+            return
+
+    if args.events:
+        device.start()
+
+    try:
+        while True:
+            await asyncio.sleep(1)
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        device.stop()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host', type=str)
+    parser.add_argument('username', type=str)
+    parser.add_argument('password', type=str)
+    parser.add_argument('-p', '--port', type=int, default=80)
+    parser.add_argument('--events', action='store_true')
+    parser.add_argument('--params', action='store_true')
+    args = parser.parse_args()
+
+    asyncio.run(main(args))
