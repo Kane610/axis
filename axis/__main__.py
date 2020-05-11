360bd2adf3b679cda7632865e7bf6c175883d4a8
Robert Svensson <Kane610@users.noreply.github.com>
2020-05-11 21:27:48 +0200

    Improve main function (#19)

--- axis/__main__.py
+++ axis/__main__.py
@@ -2,30 +2,62 @@
 
 import asyncio
 import argparse
+import async_timeout
 import logging
-import sys
 
-from axis import AxisDevice
+import axis
+
+LOGGER = logging.getLogger(__name__)
 
 
-async def main(args):
-    loop = asyncio.get_event_loop()
-    device = AxisDevice(
-        loop=loop, host=args.host, username=args.username,
-        password=args.password, port=args.port)
+def event_handler(action, event):
+    LOGGER.info(f"{action} {event}")
 
-    if args.params:
+
+async def axis_device(host, port, username, password):
+    """Create a Axis device."""
+
+    device = axis.AxisDevice(host=host, port=port, username=username, password=password)
+
+    try:
+        with async_timeout.timeout(5):
+            loop = asyncio.get_running_loop()
+            await loop.run_in_executor(None, device.vapix.initialize_users)
+
+        return device
+
+    except axis.Unauthorized:
+        LOGGER.warning("Connected to device at %s but not registered.", host)
+
+    except (asyncio.TimeoutError, axis.RequestError):
+        LOGGER.error("Error connecting to the Axis device at %s", host)
+
+    except axis.AxisException:
+        LOGGER.exception("Unknown Axis communication error occurred")
+
+
+async def main(host, port, username, password, params, events):
+    """Main function."""
+    LOGGER.info("Connecting to Axis device")
+
+    device = await axis_device(
+        host=host, username=username, password=password, port=port
+    )
+
+    if not device:
+        LOGGER.error("Couldn't connect to Axis device")
+        return
+
+    if params:
+        loop = asyncio.get_running_loop()
         await loop.run_in_executor(None, device.vapix.initialize_params)
         await loop.run_in_executor(None, device.vapix.initialize_ports)
         await loop.run_in_executor(None, device.vapix.initialize_users)
 
-        if not args.events:
+        if not events:
             return
 
-    if args.events:
-        def event_handler(action, event):
-            print(action, event)
-
+    if events:
         device.enable_events(event_callback=event_handler)
         device.start()
 
@@ -33,7 +65,7 @@ async def main(args):
         while True:
             await asyncio.sleep(1)
 
-    except KeyboardInterrupt:
+    except asyncio.CancelledError:
         pass
 
     finally:
@@ -41,14 +73,36 @@ async def main(args):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
     parser = argparse.ArgumentParser()
-    parser.add_argument('host', type=str)
-    parser.add_argument('username', type=str)
-    parser.add_argument('password', type=str)
-    parser.add_argument('-p', '--port', type=int, default=80)
-    parser.add_argument('--events', action='store_true')
-    parser.add_argument('--params', action='store_true')
+    parser.add_argument("host", type=str)
+    parser.add_argument("username", type=str)
+    parser.add_argument("password", type=str)
+    parser.add_argument("-p", "--port", type=int, default=80)
+    parser.add_argument("--events", action="store_true")
+    parser.add_argument("--params", action="store_true")
+    parser.add_argument("-D", "--debug", action="store_true")
     args = parser.parse_args()
 
-    asyncio.run(main(args))
+    loglevel = logging.INFO
+    if args.debug:
+        loglevel = logging.DEBUG
+    logging.basicConfig(format="%(message)s", level=loglevel)
+
+    LOGGER.info(
+        f"{args.host}, {args.username}, {args.password}, {args.port}, {args.events}, {args.params}"
+    )
+
+    try:
+        asyncio.run(
+            main(
+                host=args.host,
+                username=args.username,
+                password=args.password,
+                port=args.port,
+                params=args.params,
+                events=args.events,
+            )
+        )
+
+    except KeyboardInterrupt:
+        pass
--- axis/configuration.py
+++ axis/configuration.py
@@ -1,24 +1,26 @@
 """Python library to enable Axis devices to integrate with Home Assistant."""
 
+import asyncio
 import requests
 from requests.auth import HTTPDigestAuth
 
-URL = '{http}://{host}:{port}'
+URL = "{http}://{host}:{port}"
 
 
 class Configuration(object):
     """Device configuration."""
 
-    def __init__(self, *,
-                 loop, host, username, password,
-                 port=80, web_proto='http', verify_ssl=False):
+    def __init__(
+        self, *, host, username, password, port=80, web_proto="http", verify_ssl=False
+    ):
         """All config params available to the device."""
-        self.loop = loop
         self.web_proto = web_proto
         self.host = host
         self.port = port
         self.username = username
         self.password = password
+
+        self.loop = asyncio.get_running_loop()
 
         self.session = requests.Session()
         self.session.auth = HTTPDigestAuth(self.username, self.password)
