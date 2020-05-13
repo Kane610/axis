a72a317016ef70f6d8724aac5336db2015131d7a
Robert Svensson <kane610@me.com>
2020-05-13 23:27:16 +0200

    Bump to v26

--- setup.py
+++ setup.py
@@ -1,25 +1,18 @@
 """Setup for Axis."""
 
-# https://jeffknupp.com/blog/2013/08/16/open-sourcing-a-python-project-the-right-way/
-# http://peterdowns.com/posts/first-time-with-pypi.html
-# pip install -e .
-# Upload to PyPI Live
-# python setup.py sdist bdist_wheel
-# twine upload dist/axis-* --skip-existing
-
 from setuptools import setup
 
 setup(
     name="axis",
     packages=["axis"],
-    version="25",
+    version="26",
     description="A Python library for communicating with devices from Axis Communications",
     author="Robert Svensson",
     author_email="Kane610@users.noreply.github.com",
     license="MIT",
     url="https://github.com/Kane610/axis",
-    download_url="https://github.com/Kane610/axis/archive/v25.tar.gz",
+    download_url="https://github.com/Kane610/axis/archive/v26.tar.gz",
     install_requires=["requests"],
     keywords=["axis", "vapix", "onvif", "event stream", "homeassistant"],
-    classifiers=[],
+    classifiers=["Natural Language :: English", "Programming Language :: Python :: 3"],
 )
