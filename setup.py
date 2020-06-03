c0d3238e5fb43d002e0d8fd80e9b7585fb242dad
Robert Svensson <kane610@me.com>
2020-06-03 08:16:10 +0200

    Bump to v29 Fix faulty dependency attr to attrs

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup
 setup(
     name="axis",
     packages=["axis"],
-    version="28",
+    version="29",
     description="A Python library for communicating with devices from Axis Communications",
     author="Robert Svensson",
     author_email="Kane610@users.noreply.github.com",
     license="MIT",
     url="https://github.com/Kane610/axis",
-    download_url="https://github.com/Kane610/axis/archive/v28.tar.gz",
-    install_requires=["attr", "requests"],
+    download_url="https://github.com/Kane610/axis/archive/v29.tar.gz",
+    install_requires=["attrs", "requests"],
     keywords=["axis", "vapix", "onvif", "event stream", "homeassistant"],
     classifiers=["Natural Language :: English", "Programming Language :: Python :: 3"],
 )
