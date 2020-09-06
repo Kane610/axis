4d8a26f8f86c59435a72923d229ca14dd57c5921
Robert Svensson <kane610@me.com>
2020-09-06 11:25:45 +0200

    Bump to v35 Fix so applications are included in packages

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 """Setup for Axis."""
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="axis",
-    packages=["axis"],
-    version="34",
+    packages=find_packages(include=["axis", "axis.*"]),
+    version="35",
     description="A Python library for communicating with devices from Axis Communications",
     author="Robert Svensson",
     author_email="Kane610@users.noreply.github.com",
     license="MIT",
     url="https://github.com/Kane610/axis",
-    download_url="https://github.com/Kane610/axis/archive/v34.tar.gz",
+    download_url="https://github.com/Kane610/axis/archive/v35.tar.gz",
     install_requires=["attrs", "requests", "xmltodict"],
     keywords=["axis", "vapix", "onvif", "event stream", "homeassistant"],
     classifiers=["Natural Language :: English", "Programming Language :: Python :: 3"],
