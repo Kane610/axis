3cf90880d5382a222589d0bf30c6dd9b5ce1348a
Robert Svensson <Kane610@users.noreply.github.com>
2020-09-20 18:19:50 +0200

    Add packaging as dependency (#46)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     license="MIT",
     url="https://github.com/Kane610/axis",
     download_url="https://github.com/Kane610/axis/archive/v35.tar.gz",
-    install_requires=["attrs", "requests", "xmltodict"],
+    install_requires=["attrs", "packaging", "requests", "xmltodict"],
     keywords=["axis", "vapix", "onvif", "event stream", "homeassistant"],
     classifiers=["Natural Language :: English", "Programming Language :: Python :: 3"],
 )
