e6c95de7b7dc01f72de3125b53e5fa9942259e74
Robert Svensson <kane610@me.com>
2020-10-18 22:00:28 +0200

    Bump to v39 Remove python 3.8 requirement

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ from setuptools import find_packages, setup
 setup(
     name="axis",
     packages=find_packages(include=["axis", "axis.*"]),
-    version="38",
+    version="39",
     description="A Python library for communicating with devices from Axis Communications",
     author="Robert Svensson",
     author_email="Kane610@users.noreply.github.com",
     license="MIT",
     url="https://github.com/Kane610/axis",
-    download_url="https://github.com/Kane610/axis/archive/v38.tar.gz",
+    download_url="https://github.com/Kane610/axis/archive/v39.tar.gz",
     install_requires=["attrs", "packaging", "httpx", "xmltodict"],
     keywords=["axis", "vapix", "onvif", "event stream", "homeassistant"],
     classifiers=["Natural Language :: English", "Programming Language :: Python :: 3"],
-    python_requires=">=3.8.0",
 )
