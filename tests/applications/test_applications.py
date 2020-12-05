03a32959e2c965fd0aa54142af6eaee4d13310c6
Robert Svensson <kane610@me.com>
2020-12-05 19:06:40 +0100

    Improve description of device fixture

--- tests/applications/test_applications.py
+++ tests/applications/test_applications.py
@@ -15,7 +15,10 @@ from axis.device import AxisDevice
 
 @pytest.fixture
 async def device() -> AxisDevice:
-    """Returns the axis device."""
+    """Returns the axis device.
+
+    Clean up sessions automatically at the end of each test.
+    """
     axis_device = AxisDevice(Configuration("host", username="root", password="pass"))
     yield axis_device
     await axis_device.vapix.close()
