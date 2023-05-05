5f2cf0c94a1955e6e9ebf31977d105d819efe17a
Robert Svensson <Kane610@users.noreply.github.com>
2023-05-05 22:57:16 +0200

    Make configuration a dataclass (#215)

--- axis/configuration.py
+++ axis/configuration.py
@@ -1,20 +1,22 @@
 """Python library to enable Axis devices to integrate with Home Assistant."""
 
-import attr
+from dataclasses import KW_ONLY, dataclass
+
 from httpx import AsyncClient
 
 
-@attr.s
+@dataclass
 class Configuration:
     """Device configuration."""
 
-    session: AsyncClient = attr.ib()
-    host: str = attr.ib()
-    username: str = attr.ib(kw_only=True)
-    password: str = attr.ib(kw_only=True)
-    port: int = attr.ib(default=80, kw_only=True)
-    web_proto: str = attr.ib(default="http", kw_only=True)
-    verify_ssl: bool = attr.ib(default=False, kw_only=True)
+    session: AsyncClient
+    host: str
+    _: KW_ONLY
+    username: str
+    password: str
+    port: int = 80
+    web_proto: str = "http"
+    verify_ssl: bool = False
 
     @property
     def url(self) -> str:
