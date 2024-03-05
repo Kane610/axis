873d4ef66496ba4364d91fae13f69665efb608d4
Robert Svensson <Kane610@users.noreply.github.com>
2024-03-05 20:56:28 +0100

    Fix brand prod variant missing (#361)
    * Fix param prodvariant missing in brand
    
    * Fix log

--- tests/parameters/test_brand.py
+++ tests/parameters/test_brand.py
@@ -13,6 +13,14 @@ root.Brand.ProdType=Network Camera
 root.Brand.ProdVariant=
 root.Brand.WebURL=http://www.axis.com"""
 
+BRAND_5_51_RESPONSE = """root.Brand.Brand=AXIS
+root.Brand.ProdFullName=AXIS M3024-L Network Camera
+root.Brand.ProdShortName=AXIS M3024-L
+root.Brand.ProdNbr=M3024-L
+root.Brand.ProdType=Network Camera
+root.Brand.WebURL=http://www.axis.com/
+"""
+
 
 @pytest.fixture
 def brand_handler(axis_device: AxisDevice) -> BrandParameterHandler:
@@ -46,3 +54,25 @@ async def test_brand_handler(respx_mock, brand_handler: BrandParameterHandler):
     assert brand.product_type == "Network Camera"
     assert brand.product_variant == ""
     assert brand.web_url == "http://www.axis.com"
+
+
+async def test_brand_handler_5_51(respx_mock, brand_handler: BrandParameterHandler):
+    """Verify that update brand works."""
+    respx_mock.post(
+        "/axis-cgi/param.cgi",
+        data={"action": "list", "group": "root.Brand"},
+    ).respond(
+        text=BRAND_5_51_RESPONSE,
+        headers={"Content-Type": "text/plain"},
+    )
+    await brand_handler.update()
+
+    assert brand_handler.initialized
+    brand = brand_handler["0"]
+    assert brand.brand == "AXIS"
+    assert brand.product_full_name == "AXIS M3024-L Network Camera"
+    assert brand.product_number == "M3024-L"
+    assert brand.product_short_name == "AXIS M3024-L"
+    assert brand.product_type == "Network Camera"
+    assert brand.product_variant == ""
+    assert brand.web_url == "http://www.axis.com/"
