dd5a5d7d2c15c3a8b84d1a84e35d9f3219174550
Robert Svensson <Kane610@users.noreply.github.com>
2024-03-16 18:24:42 +0100

    Bump to v56 (#394)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 MIN_PY_VERSION = "3.11"
 PACKAGES = find_packages(exclude=["tests", "tests.*"])
-VERSION = "55"
+VERSION = "56"
 
 setup(
     name="axis",
