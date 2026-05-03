5f9bc8b8bb705c6e1908a45595dd8c159578a0e3
Kane610 <Kane610@users.noreply.github.com>
2026-05-03 21:28:58 +0200

    Type EventInstanceHandler with EventInstance items

--- axis/interfaces/event_instances.py
+++ axis/interfaces/event_instances.py
@@ -1,6 +1,6 @@
 """Event service and action service APIs available in Axis network device."""
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 from ..models.event_instance import (
     EventInstance,
@@ -14,14 +14,14 @@ if TYPE_CHECKING:
     from ..models.event import Event
 
 
-class EventInstanceHandler(ApiHandler[Any]):
+class EventInstanceHandler(ApiHandler[EventInstance]):
     """Event instances for Axis devices."""
 
-    async def _api_request(self) -> dict[str, Any]:
+    async def _api_request(self) -> dict[str, EventInstance]:
         """Get default data of API discovery."""
         return await self.get_event_instances()
 
-    async def get_event_instances(self) -> dict[str, Any]:
+    async def get_event_instances(self) -> dict[str, EventInstance]:
         """List all event instances."""
         bytes_data = await self.vapix.api_request(ListEventInstancesRequest())
         response = ListEventInstancesResponse.decode(bytes_data)
@@ -39,8 +39,6 @@ class EventInstanceHandler(ApiHandler[Any]):
         """
         grouped: dict[str, list[Event]] = {}
         for item in self.values():
-            if not isinstance(item, EventInstance):
-                continue
             if not include_internal_topics and item.topic in BLACK_LISTED_TOPICS:
                 continue
             grouped[item.topic] = item.to_events()
